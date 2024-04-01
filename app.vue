eb42e24fd2c06ee8f062598fe7e91b10ffa37291
vantanto <kristanto.margojoyo99@gmail.com>
2024-04-01 18:04:20 +0700

    add: google-site-verification

--- app.vue
+++ app.vue
@@ -3,7 +3,10 @@ const route = useRoute()
 
 useHead({
   title: 'VanTanto',
-  meta: [{ name: 'description', content: "I'm a software engineer in Indonesia" }],
+  meta: [
+    { name: 'description', content: "I'm a software engineer in Indonesia" },
+    { name: 'google-site-verification', content: 'zgzimCFlUXGYZd8legiYK4R2hN7Tx_42ZW6OLPvnCdY' },
+  ],
   bodyAttrs: {
     class: () => `body-${route.name?.toString()}`,
   },
